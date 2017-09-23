1751a3f879e4583c7bb357d9c47924d25b73ef1f
gudvinga <gudvinga@gmail.com>
2017-09-23 23:14:45 +0300

    Make for single brackets config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+// module.exports = 
+
+function check(str, bracketsConfig) {
+  var strArr = str.split(''),
+      steck = new Array;
+
+      for (var i = 0; i < strArr.length; i++) {
+        if(strArr[i] == bracketsConfig[0][0]) {
+          steck.push(strArr[i]); 
+        }
+
+        if(strArr[i] == bracketsConfig[0][1]) {
+          if(steck.pop() == bracketsConfig[0][0]) {}
+          else return false;
+        }
+        
+      }
+
+  // console.log(steck); 
+
+if (steck.length == 0) return true;
 }
+
+console.log(check('((()()))', [['(', ')']]));
