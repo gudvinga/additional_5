26456a9a47bda14aa3d928658899da907bccafbe
gudvinga <gudvinga@gmail.com>
2017-09-24 00:06:19 +0300

    Make find pare function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-// module.exports = 
-
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   var strArr = str.split(''),
       steck = new Array;
 
       for (var i = 0; i < strArr.length; i++) {
-        if(strArr[i] == bracketsConfig[0][0]) {
+        if(findOpen(strArr[i])) {
           steck.push(strArr[i]); 
         }
 
-        if(strArr[i] == bracketsConfig[0][1]) {
-          if(steck.pop() == bracketsConfig[0][0]) {}
+        if(!findOpen(strArr[i])) {
+          if(findPare(strArr[i])) steck.pop();
           else return false;
         }
         
       }
 
-  // console.log(steck); 
+    //find Open bracket
+    function findOpen(firstBracket) {
+      for(var i = 0; i < bracketsConfig.length; i++) {
+        if (firstBracket == bracketsConfig[i][0]) return true;
+      }
+      return false;
+    }  
+
+    //find pare
+    function findPare(curentBracket){
+      var arrPare = [steck[steck.length -1],curentBracket];  
+      for (var i = 0; i < bracketsConfig.length; i++) {
+        if(arrPare.join() == bracketsConfig[i].join()) return true;
+      }
+      return false;
+    }
+
+  console.log(steck); 
 
 if (steck.length == 0) return true;
 }
-
-console.log(check('((()()))', [['(', ')']]));
