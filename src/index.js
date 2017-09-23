df0b22d681e87d0fd6391909edd081ec308f5f13
gudvinga <gudvinga@gmail.com>
2017-09-24 00:15:54 +0300

    Problem with same  brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArr = str.split(''),
+      steck = new Array;
+
+      for (var i = 0; i < strArr.length; i++) {
+        if(findOpen(strArr[i])) {
+          steck.push(strArr[i]); 
+        }
+
+        if(!findOpen(strArr[i])) {
+          if(findPare(strArr[i])) steck.pop();
+          else return false;
+        }
+        
+      }
+
+    //find Open bracket
+    function findOpen(firstBracket) {
+      for(var i = 0; i < bracketsConfig.length; i++) {
+        if (firstBracket == bracketsConfig[i][0]) return true;
+      }
+      return false;
+    }  
+
+    //find pare
+    function findPare(curentBracket){
+      var arrPare = [steck[steck.length -1],curentBracket];  
+      for (var i = 0; i < bracketsConfig.length; i++) {
+        if(arrPare.join() == bracketsConfig[i].join()) return true;
+      }
+      return false;
+    }
+
+  console.log(steck); 
+
+if (steck.length == 0) return true;
 }
